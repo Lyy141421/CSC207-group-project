c7fb55485b34b24ed25d6dbbd976dff4a0e79add
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-06-15 10:01:14 -0400

    Setting up the daily function cyclicalTask()

--- ProjectGroup/phase1/src/JobApplicationSystem.java
+++ ProjectGroup/phase1/src/JobApplicationSystem.java
@@ -1,6 +1,24 @@
-class JobApplicationSystem {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class JobApplicationSystem {
 
     public static void main(String[] args) {
+        cyclicalTask();
+    }
 
+
+    /*
+    A method which triggers once a day from the time it is started.
+     */
+    private static void cyclicalTask(){
+        TimerTask daily_tasks = new TimerTask() {
+            public void run() {
+                // Insert any daily methods here:
+                // todo CleanUpfunction() here for example
+            }
+        };
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(daily_tasks, 0, 86400000);
     }
 }
