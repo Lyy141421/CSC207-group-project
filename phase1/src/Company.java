5302c8c86ba0fd55a8351c48ae0c368281cc72fa
esui <elaine.sui@mail.utoronto.ca>
2019-06-17 09:50:27 -0400

    JobApplicationSystem: Added instance variables, getters, setters and methods getAllJobPostings and addCompany. Company: Added some instance variables, a constructor and a getter.

--- phase1/src/Company.java
+++ phase1/src/Company.java
@@ -4,10 +4,41 @@ class Company {
 
     // Elaine: I'm adding these so that my code doesn't have red underlines
     // === Instance variables ===
+    // The total number of companies registered in this system
+    static int totalNum;
+    // The unique identifier for this company
+    private int ID;
+    // The name of this company
+    private String name;
     // A list of job postings for positions in this company.
     private ArrayList<JobPosting> jobPostings;
 
 
+    // === Constructors ===
+
+    /**
+     * Create a new company.
+     *
+     * @param name The company's name.
+     */
+    Company(String name) {
+        this.ID = totalNum;
+        this.name = name;
+        Company.totalNum++;
+    }
+
+    // === Getters ==
+
+    /**
+     * Get the job postings for positions in this company.
+     *
+     * @return the list of job postings for positions in this company.
+     */
+    ArrayList<JobPosting> getJobPostings() {
+        return this.jobPostings;
+    }
+
+    // === Other methods ===
     Applicant getFinalCandidate(JobPosting jobPosting) {
         return null;
     }
--- phase1/src/JobApplicationSystem.java
+++ phase1/src/JobApplicationSystem.java
@@ -4,11 +4,57 @@ import java.util.TimerTask;
 
 public class JobApplicationSystem {
 
+    // === Instance variables ===
+    // List of companies registered in the system
+    static ArrayList<Company> companies = new ArrayList<>();
+    // The user manager for the system
+    static UserManager userManager;
+
     public static void main(String[] args) {
         cyclicalTask();
     }
 
+    // === Getters ===
 
+    /**
+     * Get a list of companies registered in this system.
+     *
+     * @return a list of companies registered in this system.
+     */
+    static ArrayList<Company> getCompanies() {
+        return JobApplicationSystem.companies;
+    }
+
+    /**
+     * Get the UserManager for this system.
+     *
+     * @return the UserManager for this system.
+     */
+    static UserManager getUserManager() {
+        return JobApplicationSystem.userManager;
+    }
+
+    // === Setters ===
+
+    /**
+     * Set the companies using this system.
+     *
+     * @param companies The companies using this system.
+     */
+    static void setCompanies(ArrayList<Company> companies) {
+        JobApplicationSystem.companies = companies;
+    }
+
+    /**
+     * Set the user manager for this system.
+     *
+     * @param userManager The user manager for this system.
+     */
+    static void setUserManager(UserManager userManager) {
+        JobApplicationSystem.userManager = userManager;
+    }
+
+    // === Other methods ===
     /*
     A method which triggers once a day from the time it is started.
      */
@@ -23,7 +69,25 @@ public class JobApplicationSystem {
         timer.scheduleAtFixedRate(daily_tasks, 0, 86400000);
     }
 
+    /**
+     * Get all the job postings in this system.
+     * @return all the job postings in this system.
+     */
     static ArrayList<JobPosting> getAllJobPostings() {
-        return null;
+        ArrayList<JobPosting> jobPostings = new ArrayList<>();
+        for (Company company : JobApplicationSystem.companies) {
+            jobPostings.addAll(company.getJobPostings());
+        }
+        return jobPostings;
+    }
+
+    /**
+     * Add a company to the system.
+     *
+     * @param name The name of the company.
+     * @return the company with this name.
+     */
+    static Company addCompany(String name) {
+        return new Company(name);
     }
 }
