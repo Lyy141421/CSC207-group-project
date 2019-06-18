8c0522468377b0bd2c0b37b30b9566e01e2d155d
wangpopl <poplar.wang@mail.utoronto.ca>
2019-06-18 19:17:20 -0400

    User instantiation and operations in UserManager

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.time.LocalDate;
 
 class UserManager {
     // Class that stores, creates and updates children of the User class
@@ -6,11 +7,55 @@ class UserManager {
     // === Instance Variables ===
     private User[] allUsers = new User[50]; //Array in which all users are stored
 
-    UserManager(BufferedReader usersFile) {
-        //TODO: instantiate existing users upon startup by reading usersFile
+    UserManager(BufferedReader appUsers) {
+        //TODO: instantiate existing users upon startup by reading appUsers
+    }
+
+    // === Creating User accounts and adding them to allUsers ===
+    private void createInterviewer(String email, Company company, String field, LocalDate dateCreated) {
+        Interviewer newInterviewer = new Interviewer(email, company, field, dateCreated);
+        this.addToAllUsers(newInterviewer);
+    }
+
+    private void createHRCoordinator(String username, String password, String legalName,
+                                     String email, Company company, LocalDate dateCreated) {
+        HRCoordinator newHRBoy = new HRCoordinator(username, password, legalName, email, company, dateCreated);
+        this.addToAllUsers(newHRBoy);
+    }
+
+    private void createApplicant(String username, String password,
+                                 String legalName, String email, LocalDate dateCreated) {
+        Applicant newApplicant = new Applicant(username, password, legalName, email, dateCreated);
+        this.addToAllUsers(newApplicant);
+    }
+
+    // === User Operations ===
+
+    /**
+     * Checks if the password submitted matches the User's password, returns boolean
+     */
+    boolean login(User checkedUser, String password) {
+        return checkedUser.getPassword().equals(password);
     }
 
     // === Array operations on allUsers ===
+    /**
+     * @param username Account being searched for
+     * @return Account with the provided username. >RETURNS A DEFAULT APPLICANT IF NONE FOUND<
+     * TODO: Figure out a better way to deal with non-existent accounts
+     */
+    User findUserByUsername(String username) {
+        User returnUser = new Applicant(); //Returned default account
+        for(int i=0; i < allUsers.length - 1; i++) {
+            if(allUsers[i] == null) {
+                break;
+            } else if(allUsers[i].getUsername().equals(username)) {
+                returnUser = allUsers[i];
+                break;
+            }
+        }
+        return returnUser;
+    }
 
     /**
      * Adds the passed user to allUsers. Checks and expands allUsers size when required.
