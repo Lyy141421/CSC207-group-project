087a8c8cbe57ebb2498ff430acc4bb6b4d38ad04
yiyan <yiyan.luo@mail.utoronto.ca>
2019-06-18 23:52:34 -0400

    InterviewManager: Split interviews into ongoingInterviews and completedInterviews. Added method completeInterview to move ongoing to completed.

--- phase1/src/InterviewManager.java
+++ phase1/src/InterviewManager.java
@@ -5,7 +5,8 @@ class InterviewManager {
     // Elaine --- I'm doing this so that my code won't be underlined red
     // === Instance variables ===
     // List of interviews
-    private ArrayList<Interview> interviews = new ArrayList<Interview>();
+    private ArrayList<Interview> ongoingInterviews = new ArrayList<Interview>();
+    private ArrayList<Interview> completedInterviews = new ArrayList<Interview>();
     private ArrayList<Applicant> applicantsInConsideration = new ArrayList<Applicant>();
     private ArrayList<Applicant> applicantsRejected = new ArrayList<Applicant>();
     private int currentRound;
@@ -14,7 +15,12 @@ class InterviewManager {
     // === Other methods ===
 
     void addInterview(Interview interview) {
-        this.interviews.add(interview);
+        this.ongoingInterviews.add(interview);
+    }
+
+    void completeInterview(Interview interview) {
+        this.ongoingInterviews.remove(interview);
+        this.completedInterviews.add(interview);
     }
 
     void reject (Applicant applicant) {
