8a75614f37c9d3de7d231556c462fbb3028d08f2
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-06-22 12:50:09 -0400

    FileSystem FileToJson Added

--- phase1/src/FileSystem.java
+++ phase1/src/FileSystem.java
@@ -8,4 +8,23 @@ import org.json.simple.parser.*;
 import java.util.Iterator;
 
 public class FileSystem {
+
+    /**
+     *Used to create or load JSON Objects form a filename
+     *
+     * @param filename The name of the .json file to be accessed (Only the name not including .json)
+     * @return The JSON object loaded from the .json file
+     */
+    static private JSONObject FileToJson(String filename){
+        Object obj = null;
+        JSONObject jobj = null;
+        try {
+            obj = new JSONParser().parse(new FileReader(filename + ".json"));
+            jobj = new JSONObject(obj.toString());
+        } catch (IOException | ParseException | JSONException e) {
+            jobj = new JSONObject();
+            e.printStackTrace();
+        }
+        return jobj;
+    }
 }
