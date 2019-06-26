95700cc087c8172c252e56812ef14b4d81be5aa4
yiyan <yiyan.luo@mail.utoronto.ca>
2019-06-25 22:49:22 -0400

    MainFrame: Moved code to new initUI method, set layout to CardLayout.

--- phase1/src/MainFrame.java
+++ phase1/src/MainFrame.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
     public MainFrame () {
         super("GET A JOB");
+        initUI();
+    }
+
+    private void initUI () {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 450);
+        setLayout(new CardLayout());
         setVisible(true);
     }
 }
