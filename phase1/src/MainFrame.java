a3131e48db448fb2cc4a1fb0618835817228792a
yiyan <yiyan.luo@mail.utoronto.ca>
2019-06-26 12:58:05 -0400

    MainFrame: Added method addCards, started HRInterface panel.

--- phase1/src/MainFrame.java
+++ phase1/src/MainFrame.java
@@ -12,6 +12,19 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 450);
         setLayout(new CardLayout());
+        addCards();
         setVisible(true);
+    }
+
+    // Call methods that create each interface and add to main frame.
+    private void addCards () {
+        add(HRInterface());
+    }
+
+    // Create interface for HR
+    private JPanel HRInterface () {
+        JPanel HRPanel = new JPanel();
+        HRPanel.setLayout(new CardLayout());
+        return HRPanel;
     }
 }
