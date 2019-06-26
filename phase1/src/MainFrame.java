22ac5504a084ae18a0f040adf096494524272636
yiyan <yiyan.luo@mail.utoronto.ca>
2019-06-26 15:42:51 -0400

    MainFrame: Implemented HRHome, Added HRHome panel to HRInterface.

--- phase1/src/MainFrame.java
+++ phase1/src/MainFrame.java
@@ -23,8 +23,56 @@ public class MainFrame extends JFrame {
 
     // Create interface for HR
     private JPanel HRInterface () {
+        setTitle("HR Coordinator");
         JPanel HRPanel = new JPanel();
         HRPanel.setLayout(new CardLayout());
+        HRPanel.add(HRHome());
+
         return HRPanel;
+    }
+
+    private JPanel HRHome () {
+        JPanel HomePanel = new JPanel();
+        HomePanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        JButton toDo = new JButton("To-Do");
+
+        c.gridx = 0;
+        c.gridy = 0;
+        HomePanel.add(toDo, c);
+
+
+        JPanel manual = new JPanel();
+        manual.setLayout(new BoxLayout(manual, BoxLayout.Y_AXIS));
+        manual.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1, true));
+
+        JButton browsePosting = new JButton("Browse all job postings");
+        JButton searchApplicant = new JButton("Search applicant");
+        JButton addPosting = new JButton("Add job posting");
+
+        browsePosting.setAlignmentX(Component.CENTER_ALIGNMENT);
+        searchApplicant.setAlignmentX(Component.CENTER_ALIGNMENT);
+        addPosting.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        manual.add(addPosting);
+        manual.add(browsePosting);
+        manual.add(searchApplicant);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        HomePanel.add(manual, c);
+
+        JButton logout = new JButton("Logout");
+
+        c.gridx = 2;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.insets = new Insets(20, 100, 0, 0);
+        HomePanel.add(logout, c);
+
+
+        return HomePanel;
     }
 }
