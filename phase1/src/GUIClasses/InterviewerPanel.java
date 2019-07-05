4506d3e9bc37cc43ed606ae64a1102a2ddb1fce4
yiyan <yiyan.luo@mail.utoronto.ca>
2019-07-04 20:44:32 -0400

    InterviewPanel: Implemented home and viewInterviews panels. MainFrame: Comment on addCards method.

--- phase1/src/GUIClasses/InterviewerPanel.java
+++ phase1/src/GUIClasses/InterviewerPanel.java
@@ -12,10 +12,19 @@ public class InterviewerPanel extends JPanel implements ActionListener {
         this.add(home(), "HOME");
         this.add(viewInterviews(), "VIEW");
         this.add(reviewInterviews(), "REVIEW");
+        this.add(scheduleInterviews(), "SCHEDULE");
     }
 
     private JPanel home () {
         JPanel homePanel = new JPanel();
+
+        JButton view = new JButton("View scheduled interviews");
+        JButton review = new JButton("Update interview result");
+        JButton schedule = new JButton("Schedule interview");
+
+        homePanel.add(view);
+        homePanel.add(review);
+        homePanel.add(schedule);
 
         return homePanel;
     }
@@ -23,17 +32,42 @@ public class InterviewerPanel extends JPanel implements ActionListener {
     private JPanel viewInterviews () {
         JPanel viewPanel = new JPanel();
 
+        JComboBox<String> interviews = new JComboBox<>();
+        JList<String> viewable = new JList<>(new String[]{"abc", "xa"});
+        JTextArea info = new JTextArea("Information");
+        info.setEditable(false);
+        // Phase 2?
+        // JButton reschedule = new JButton("Re-schedule");
+        JButton home = new JButton("Home");
+        home.addActionListener(this);
+
+        JSplitPane display = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(viewable), new JScrollPane(info));
+        display.setDividerLocation(250);
+
+        viewable.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        viewable.setLayoutOrientation(JList.VERTICAL);
+
+        viewPanel.add(interviews, BorderLayout.NORTH);
+        viewPanel.add(display, BorderLayout.CENTER);
+        viewPanel.add(home, BorderLayout.SOUTH);
+
         return viewPanel;
     }
 
-    private JPanel reviewInterviews() {
+    private JPanel reviewInterviews () {
         JPanel reviewPanel = new JPanel();
 
         return reviewPanel;
     }
 
+    private JPanel scheduleInterviews () {
+        JPanel schedulePanel = new JPanel();
+
+        return schedulePanel;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 }
--- phase1/src/GUIClasses/MainFrame.java
+++ phase1/src/GUIClasses/MainFrame.java
@@ -23,6 +23,7 @@ public class MainFrame extends JFrame {
 
     // Call methods that create each interface and add to main frame.
     private void addCards () {
+        // We need to be careful with when these cards get constructed, in case it's missing arguments to run methods
         add(new LoginPanel(), "LOGIN");
         add(new NewUserPanel(), "NEWUSER");
         add(new HRPanel(), "HR");
