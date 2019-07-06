4884ae6584f3609bb8c8adea8ff6783d292cc8e4
esui <elaine.sui@mail.utoronto.ca>
2019-07-05 21:53:10 -0400

    Changed the implementation of InterfaceFactory so that the class names are not hard coded.

--- phase1/src/GUIClasses/InterfaceFactory.java
+++ phase1/src/GUIClasses/InterfaceFactory.java
@@ -4,6 +4,14 @@ import UsersAndJobObjects.User;
 
 public class InterfaceFactory implements AbstractFactory<UserInterface> {
 
+    // === Class variables ===
+    // The name of the applicant class
+    private static final String APPLICANT_CLASS_NAME = "UsersAndJobObjects.Applicant";
+    // The name of the interviewer class
+    private static final String INTERVIEWER_CLASS_NAME = "UsersAndJobObjects.Interviewer";
+    // The name of the HR coordinator class
+    private static final String HRCOORDINATOR_CLASS_NAME = "UsersAndJobObjects.HRCoordinator";
+
     /**
      * Create the appropriate interface for this user.
      * @param object  The user who logged in.
@@ -12,11 +20,11 @@ public class InterfaceFactory implements AbstractFactory<UserInterface> {
     public UserInterface create(Object object) {
         User user = (User) object;
         switch (user.getClass().getName()) {
-            case "UsersAndJobObjects.Applicant":
+            case InterfaceFactory.APPLICANT_CLASS_NAME:
                 return new ApplicantInterface(user);
-            case "UsersAndJobObjects.Interviewer":
+            case InterfaceFactory.INTERVIEWER_CLASS_NAME:
                 return new InterviewerInterface(user);
-            case "UsersAndJobObjects.HRCoordinator":
+            case InterfaceFactory.HRCOORDINATOR_CLASS_NAME:
                 return new HRCoordinatorInterface(user);
         }
         return null;
