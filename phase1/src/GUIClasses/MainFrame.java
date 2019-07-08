1bd30a427375839ced3fd46c09db46fa5438882f
yiyan <yiyan.luo@mail.utoronto.ca>
2019-07-08 12:28:48 -0400

    MainFrame: Trying to eliminate error.

--- phase1/src/GUIClasses/MainFrame.java
+++ phase1/src/GUIClasses/MainFrame.java
@@ -8,18 +8,19 @@ public class MainFrame extends JFrame {
 
     LocalDate today;
     private CardLayout layoutManager = new CardLayout();
-    private JPanel homePanel; NewUserPanel newUserRef;
+    //private JPanel homePanel; NewUserPanel newUserRef;
 
     public MainFrame (LocalDate today) {
         super("GET A JOB");
         this.today = today;
         initUI();
+        addCards();
     }
 
     private MainFrame () {
         super("TESTING INSTANTIATOR DONT USE");
-        this.homePanel = new JPanel(this.layoutManager);
-        this.add(homePanel);
+        //this.homePanel = new JPanel(this.layoutManager);
+        //this.add(homePanel);
         initUI();
         addCards();
     }
@@ -30,6 +31,7 @@ public class MainFrame extends JFrame {
     private void initUI () {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(854, 480);
+        this.getContentPane().setLayout(new CardLayout());
         addCards();
         setVisible(true);
         setResizable(false);
@@ -42,16 +44,16 @@ public class MainFrame extends JFrame {
     private void addCards () {
         //this.add(this.homePanel);
         // We need to be careful with when these cards get constructed, in case it's missing arguments to run methods
-        this.homePanel.add(new LoginPanel(this.homePanel, this.layoutManager), "LOGIN");
+        this.add(new LoginPanel(this.homePanel, this.layoutManager), "LOGIN");
         this.newUserRef = new NewUserPanel(this.homePanel, this.layoutManager);
-        this.homePanel.add(this.newUserRef, "NEWUSER");
+        this.add(this.newUserRef, "NEWUSER");
         // got User object from login/signup
         //interface = new InterfaceFactory.create(User)
         //if User instanceOf ...
         //(cast to xxxInterface) interface
-//        add(new HRPanel(interface, today), "HR");
-//        add(new InterviewerPanel(), "INTERVIEWER");
-//        add(new ApplicantPanel(), "APPLICANT");
+//        this.add(new HRPanel(interface, today), "HR");
+//        this.add(new InterviewerPanel(interface, today), "INTERVIEWER");
+//        this.add(new ApplicantPanel(), "APPLICANT");
     }
 
     public static void main(String[] args) {
