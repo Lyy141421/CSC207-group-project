b1b6077042aebb0ca48fca7364935a58aee9625b
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-07-09 18:44:10 -0400

    Pushing Error for Debugging

--- phase1/src/Main/JobApplicationSystem.java
+++ phase1/src/Main/JobApplicationSystem.java
@@ -2,6 +2,7 @@ package Main;
 
 import FileLoadingAndStoring.*;
 import GUIClasses.MainFrame;
+import Managers.JobPostingManager;
 import UsersAndJobObjects.Company;
 import UsersAndJobObjects.Interview;
 import UsersAndJobObjects.JobApplication;
@@ -36,6 +37,8 @@ public class JobApplicationSystem {
 
     public static void main(String[] args) {
         mainStart();
+        ((HRCoordinator)userManager.findUserByUsername("Stacy")).addJobPosting("New HR Manager", "HR", "I want out so have this", "Not a single one", 1, LocalDate.now(), LocalDate.now().plusDays(5));
+        companies.get(0).getJobPostingManager().getJobPostings().get(1);
         cyclicalTask();
         SwingUtilities.invokeLater(new Runnable() {
             @Override
