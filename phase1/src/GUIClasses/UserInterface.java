761d4934c1d87fb1ad74ccf94e7acdb1eb12f486
esui <elaine.sui@mail.utoronto.ca>
2019-07-10 14:26:42 -0400

    Changed some comments.

--- phase1/src/GUIClasses/UserInterface.java
+++ phase1/src/GUIClasses/UserInterface.java
@@ -18,7 +18,6 @@ public class UserInterface {
     // The user who logged in
     User user;
 
-    // === Public methods ===
     // === Constructor ===
     UserInterface(User user) {
         this.user = user;
@@ -27,8 +26,6 @@ public class UserInterface {
     // ============================================================================================================== //
     // === Public methods ===
     // === Constructor ===
-    public UserInterface() {
-    }
 
     // === Method to be overridden ===
     /**
