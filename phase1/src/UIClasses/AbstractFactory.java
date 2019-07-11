41b8fb87eef94ec2a212d0333fc0c65551592103
esui <elaine.sui@mail.utoronto.ca>
2019-07-10 20:21:48 -0400

    Revert: deleted AbstractFactory and InterfaceFactory (lazy classes) and moved contents to UserInterface

---
+++ phase1/src/UIClasses/AbstractFactory.java
@@ -0,0 +1,16 @@
+package UIClasses;
+
+interface AbstractFactory<T> {
+    /**
+     * Factory that creates appropriate objects.
+     */
+
+    /**
+     * Creates the appropriate object based off of the parameter type.
+     *
+     * @param object The object for which something is to be created for.
+     * @return the object created.
+     */
+    T create(Object object);
+
+}
