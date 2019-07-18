9cec90e322440d72786105d7c41f86e54ddb86b4
esui <elaine.sui@mail.utoronto.ca>
2019-07-18 17:29:01 -0400

    Updated the document manager for companies and removed any hardcoded file paths.

---
+++ phase2/src/ActionListeners/ApplicantActionListeners/BrowseJobPostingsActionListener.java
@@ -0,0 +1,17 @@
+package ActionListeners.ApplicantActionListeners;
+
+import ApplicantStuff.Applicant;
+
+import java.awt.event.ActionEvent;
+
+public class BrowseJobPostingsActionListener extends ApplicantActionListener {
+
+    public BrowseJobPostingsActionListener(Applicant applicant) {
+        super(applicant);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
