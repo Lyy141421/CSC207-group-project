235405d77efdfe0741ac4a1b0489208582ce3094
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-07-20 19:42:30 -0400

    Adding Methods to NotificationManager

--- phase2/src/NotificationSystem/NotificationManager.java
+++ phase2/src/NotificationSystem/NotificationManager.java
@@ -14,4 +14,18 @@ public class NotificationManager {
     public void NotificationManager(ArrayList<Notification> list){
         this.notification_list = list;
     }
+
+    // === Other Methods ===
+
+    public void add(Notification notification){this.notification_list.add(notification);}
+
+    public void remove(Notification notification){this.notification_list.remove(notification);}
+
+    public String displayAll(){
+        StringBuilder output = new StringBuilder();
+        for (Notification notification : this.notification_list){
+            output.append(notification.toString());
+        }
+        return output.toString();
+    }
 }
