37032bc992eb2caa16cf74aa0d736d21e5d65fdc
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-07-21 11:19:37 -0400

    Adding mass add notifications to notification manager

--- phase2/src/NotificationSystem/NotificationManager.java
+++ phase2/src/NotificationSystem/NotificationManager.java
@@ -19,6 +19,12 @@ public class NotificationManager {
 
     public void add(Notification notification){this.notification_list.add(notification);}
 
+    public void add(ArrayList<Notification> notification_list){
+        for (Notification notification : notification_list){
+            this.add(notification);
+        }
+    }
+
     public void remove(Notification notification){this.notification_list.remove(notification);}
 
     public String displayAll(){
