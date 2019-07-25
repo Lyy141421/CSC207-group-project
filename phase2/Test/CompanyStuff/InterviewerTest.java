93d0bb284b526e7a637be43a30fd507d192e6fff
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-07-25 15:50:33 -0400

    Moving and correcting interviewer test

--- phase2/Test/CompanyStuff/InterviewerTest.java
+++ phase2/Test/CompanyStuff/InterviewerTest.java
@@ -1,5 +1,6 @@
 package CompanyStuff;
 
+import NotificationSystem.NotificationManager;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -15,6 +16,22 @@ class InterviewerTest {
     Interviewer createInterviewer (String username){
         return new Interviewer(username, "ABC123", username + "'s Real Name", username + "@gmail.com", branch_a,
                 "Sales", LocalDate.of(2019, 7, 29));
+    }
+
+    @Test
+    void testConstructor() {
+        Interviewer interviewer = this.createInterviewer("Phillip");
+
+        assertEquals(interviewer.getUsername(), "Phillip");
+        assertEquals(interviewer.getPassword(), "ABC123");
+        assertEquals(interviewer.getLegalName(), "Phillip's Real Name");
+        assertEquals(interviewer.getEmail(), "Phillip@gmail.com");
+        assertEquals(interviewer.getDateCreated(), LocalDate.of(2019, 7, 29));
+
+        assertTrue(interviewer.getNotificationManager() instanceof NotificationManager);
+        assertEquals(interviewer.getBranch().getName(), "BranchName");
+        assertEquals(interviewer.getBranch().getCma(), "Skiff Lake");
+        assertEquals(interviewer.getField(), "Sales");
     }
 
     @Test
