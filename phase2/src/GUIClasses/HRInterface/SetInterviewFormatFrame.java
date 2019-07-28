135b6cdb02fa3f5b1409f7df205eb71c8ae7bcb0
yiyan <yiyan.luo@mail.utoronto.ca>
2019-07-28 17:52:29 -0400

    SetInterviewFormatFrame: Changed layout to keep buttons on the bottom; added lists to keep track to input fields.

--- phase2/src/GUIClasses/HRInterface/SetInterviewFormatFrame.java
+++ phase2/src/GUIClasses/HRInterface/SetInterviewFormatFrame.java
@@ -9,13 +9,24 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class SetInterviewFormatFrame extends JInternalFrame {
 
-    int rounds = 1;
+    private int rounds = 0;
+
+    ArrayList<JRadioButton> oneOnOneButtonList = new ArrayList<>();
+    ArrayList<JTextField> descriptionInputList = new ArrayList<>();
+
+    JPanel formatPanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
 
     SetInterviewFormatFrame(MethodsTheGUICallsInHR HRInterface, BranchJobPosting branchJobPosting) {
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setLayout(new BorderLayout());
+        this.formatPanel.setLayout(new BoxLayout(this.formatPanel, BoxLayout.Y_AXIS));
+        this.add(new JScrollPane(this.formatPanel), BorderLayout.CENTER);
+        this.buttonPanel.setLayout(new BorderLayout());
+        this.add(this.buttonPanel, BorderLayout.SOUTH);
 
         this.addSelectPanel();
         this.addNewRoundButton();
@@ -23,6 +34,8 @@ public class SetInterviewFormatFrame extends JInternalFrame {
     }
 
     void addSelectPanel() {
+        this.rounds++;
+
         JPanel selectPanel = new JPanel(new GridBagLayout());
         selectPanel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED), "Interview round " + this.rounds));
         GridBagConstraints c = new GridBagConstraints();
@@ -34,6 +47,7 @@ public class SetInterviewFormatFrame extends JInternalFrame {
         ButtonGroup buttonGroup = new ButtonGroup();
         c.gridx++;
         JRadioButton oneOnOne = new JRadioButton("One-on-One", true);
+        this.oneOnOneButtonList.add(oneOnOne);
         selectPanel.add(oneOnOne, c);
         c.gridx++;
         JRadioButton group = new JRadioButton("Group");
@@ -48,9 +62,10 @@ public class SetInterviewFormatFrame extends JInternalFrame {
         c.gridx++;
         c.gridwidth = 2;
         JTextField descriptionInput = new JTextField();
+        this.descriptionInputList.add(descriptionInput);
         selectPanel.add(descriptionInput, c);
 
-        this.add(selectPanel);
+        this.formatPanel.add(selectPanel);
     }
 
     void addNewRoundButton() {
@@ -59,20 +74,25 @@ public class SetInterviewFormatFrame extends JInternalFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 addSelectPanel();
-                //TODO: shift the action buttons down to make room for new selection panel
             }
         });
 
-        this.add(newRoundButton);
+        this.buttonPanel.add(newRoundButton, BorderLayout.CENTER);
     }
 
     void addSubmitAndCancel() {
-        JPanel submitAndCancelPanel = new JPanel(new FlowLayout());
+        JPanel submitAndCancelPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
         JButton cancelButton = new JButton("Cancel");
         submitAndCancelPanel.add(submitButton);
         submitAndCancelPanel.add(cancelButton);
 
-        this.add(submitAndCancelPanel);
+        this.buttonPanel.add(submitAndCancelPanel, BorderLayout.SOUTH);
     }
 }
