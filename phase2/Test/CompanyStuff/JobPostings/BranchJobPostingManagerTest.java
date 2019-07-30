4c2e91c8cba9c33626ea46fdea1429c25cb24141
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-07-29 21:51:41 -0400

    Starting BranchJobPostingManager Test

---
+++ phase2/Test/CompanyStuff/JobPostings/BranchJobPostingManagerTest.java
@@ -0,0 +1,76 @@
+package CompanyStuff.JobPostings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BranchJobPostingManagerTest {
+
+    @Test
+    void getBranchJobPosting() {
+    }
+
+    @Test
+    void getBranchJobPostings() {
+    }
+
+    @Test
+    void addJobPosting() {
+    }
+
+    @Test
+    void getJobPostingsNotAppliedToByApplicant() {
+    }
+
+    @Test
+    void getOpenJobPostings() {
+    }
+
+    @Test
+    void getClosedJobPostingsNotFilled() {
+    }
+
+    @Test
+    void getJobPostingsRecentlyClosedForApplications() {
+    }
+
+    @Test
+    void getJobPostingsRecentlyClosedForReferences() {
+    }
+
+    @Test
+    void getJobPostingsThatNeedInterviewProcessConfigured() {
+    }
+
+    @Test
+    void getFilledJobPostings() {
+    }
+
+    @Test
+    void getJobPostingsThatNeedGroupInterviewsScheduled() {
+    }
+
+    @Test
+    void getJobPostingsThatNeedHRSelectionForHiring() {
+    }
+
+    @Test
+    void updateAllClosedUnfilledJobPostings() {
+    }
+
+    @Test
+    void getBranch() {
+    }
+
+    @Test
+    void setBranch() {
+    }
+
+    @Test
+    void updateJobPostingsClosedForApplications() {
+    }
+
+    @Test
+    void updateJobPostingsClosedForReferences() {
+    }
+}
