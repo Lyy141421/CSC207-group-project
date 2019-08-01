e951a6ef743dd2556bd8f223058f8dab16beb1e0
Phillip Soetebeer <Phillip.soetebeer@mail.utoronto.ca>
2019-08-01 12:23:43 -0400

    Fixing typo in Notification Message

--- phase2/src/CompanyStuff/InterviewManager.java
+++ phase2/src/CompanyStuff/InterviewManager.java
@@ -201,8 +201,8 @@ public class InterviewManager extends Observable implements Serializable {
         if (this.hasNoJobApplicationsInConsideration()) {
             this.updateObserverList();
             this.notifyAllObservers(new Notification("Warning: No Applications in Consideration",
-                    "There are no applications in consideration in " + this.getBranchJobPosting().getTitle()
-                            + " job posting (id " + this.getBranchJobPosting().getId() + ")."));
+                    "There are no applications in consideration for the " + this.getBranchJobPosting().getTitle()
+                            + " job posting."));
         } else if (this.currentRound != 0 | this.isInterviewProcessOver()) {
             // The check for current round ensures that applicants get at least 1 interview
             this.hireAllApplicants();
