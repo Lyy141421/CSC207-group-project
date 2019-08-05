eb117642fc67aee33459cb3fde6e93d9e79c53d9
wangpopl <poplar.wang@mail.utoronto.ca>
2019-08-05 17:00:19 -0400

    bugfix

--- phase2/src/GUIClasses/ApplicantInterface/ApplicantPanel.java
+++ phase2/src/GUIClasses/ApplicantInterface/ApplicantPanel.java
@@ -5,7 +5,6 @@ import GUIClasses.ActionListeners.LogoutActionListener;
 import GUIClasses.CommonUserGUI.UserMain;
 import Main.JobApplicationSystem;
 
-import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -16,7 +15,6 @@ public class ApplicantPanel extends UserMain {
     private Applicant applicant;
     private JobApplicationSystem jobAppSystem;
     private LogoutActionListener logout;
-    private JPanel cards = new JPanel(new CardLayout());
 
     public ApplicantPanel(Applicant applicant, JobApplicationSystem jobAppSystem, LogoutActionListener logout) {
         this.setLayout(new CardLayout());
@@ -24,17 +22,16 @@ public class ApplicantPanel extends UserMain {
         this.jobAppSystem = jobAppSystem;
         this.logout = logout;
         this.setCards();
-        this.add(cards);
     }
 
     public void setCards() {
-        cards.add(new ApplicantMain(this.applicant, this, this.jobAppSystem, this.logout), "Main");
-        cards.add(new ApplicantViewApps(), "SearchResults");
+        this.add(new ApplicantMain(this.applicant, this, this.jobAppSystem, this.logout), "Main");
+        this.add(new ApplicantViewApps(), "SearchResults");
     }
 
     // Abstract method in UserMain
     public void refresh() {
-        cards.removeAll();
+        this.removeAll();
         this.setCards();
     }
 }
