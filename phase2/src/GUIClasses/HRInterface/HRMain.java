eaad23a62e6124b8646a73edb52364ce73008dff
yiyan <yiyan.luo@mail.utoronto.ca>
2019-08-08 10:44:22 -0400

    HRMain: removed redundant test code.

--- phase2/src/GUIClasses/HRInterface/HRMain.java
+++ phase2/src/GUIClasses/HRInterface/HRMain.java
@@ -142,109 +142,38 @@ public class HRMain extends UserMain {
         return this.allJP;
     }
 
-//    public static void main(String[] args) {//Phillips Test For Lif Cycle 2
-//        LocalDate refdate = LocalDate.now();
-//        JFrame frame = new JFrame("HR Main");
-//        JobApplicationSystem jobApplicationSystem = new JobApplicationSystem();
-//        new DataLoaderAndStorer(jobApplicationSystem).loadAllData();
-//        jobApplicationSystem.setToday(refdate.plusDays(7));
-//        jobApplicationSystem.setPreviousLoginDate(refdate.plusDays(-1));
-//        Reference reference = jobApplicationSystem.getUserManager().createReference("ref@gmail.com", refdate.plusDays(-1));
-//        Company company = jobApplicationSystem.createCompany("Segufix");
-//        Branch branch = company.createBranch("Segufix Canada", "E6H1P9");
-//        Applicant applicant = jobApplicationSystem.getUserManager().createApplicant("Fran", "1", "Hannah J", "Fran@mail.com", "E6H1P9", jobApplicationSystem);
-//        Applicant applicant1 = jobApplicationSystem.getUserManager().createApplicant("Will", "1", "Will W", "Will@mail.com", "E6H1P9", jobApplicationSystem);
-//        Applicant applicant2 = jobApplicationSystem.getUserManager().createApplicant("Jon", "1", "Jon J", "Jon@mail.com", "E6H1P9", jobApplicationSystem);
-//        Interviewer interviewer = jobApplicationSystem.getUserManager().createInterviewer("Stacy", "1", "Stacy O", "Stacy@mail.com", "Sales", branch, jobApplicationSystem);
-//        HRCoordinator hrc = jobApplicationSystem.getUserManager().createHRCoordinator("Phil", "1", "Phillip Soetebeer", "email@gmail.com", branch, jobApplicationSystem);
-//        BranchJobPosting jobPosting = hrc.addJobPosting("Posting 1", "Sales", "A Poor Choice",
-//                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(Arrays.asList("Tags", "Tag2")), 2, refdate, refdate.plusDays(5));
-//        JobApplication jobApp = new JobApplication(applicant, jobPosting, refdate);
-//        JobApplication jobApp1 = new JobApplication(applicant1, jobPosting, refdate);
-//        JobApplication jobApp2 = new JobApplication(applicant2, jobPosting, refdate);
-//        JobApplicationDocument doc = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        JobApplicationDocument doc2 = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        jobApp.addFiles(new ArrayList<>(Arrays.asList(doc, doc2)));
-//        jobApp1.addFiles(new ArrayList<>(Arrays.asList(doc, doc2)));
-//        jobApp2.addFiles(new ArrayList<>(Arrays.asList(doc, doc2)));
-//        reference.addJobApplication(jobApp);
-//        reference.addJobApplication(jobApp1);
-//        reference.addJobApplication(jobApp2);
-//        BranchJobPostingManager branchJobPostingManager = branch.getJobPostingManager();
-//        branchJobPostingManager.updateJobPostingsClosedForApplications(refdate.plusDays(6));
-//        System.out.println("AAAAAAAAAAAAAAAA:"+branch.getJobPostingManager().getBranchJobPostings().get(0).getInterviewManager().getHrTask());
-//        LogoutActionListener logoutActionListener = new LogoutActionListener(new Container(), new CardLayout(), jobApplicationSystem);
-//        frame.add(new HRMain(hrc, jobApplicationSystem, logoutActionListener));
-//        frame.setSize(854, 480);
-//        frame.setVisible(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    }
 
-
-//    public static void main(String[] args) {
-//        JFrame frame = new JFrame("HR Main");
-//        JobApplicationSystem jobApplicationSystem = new JobApplicationSystem();
-//        //new DataLoaderAndStorer(jobApplicationSystem).loadAllData();
-//        jobApplicationSystem.setToday(LocalDate.of(2019, 7, 28));
-//        jobApplicationSystem.setPreviousLoginDate(LocalDate.of(2019, 7, 19));
-//        Reference reference = jobApplicationSystem.getUserManager().createReference("bob@gmail.com", LocalDate.of(2019, 7, 19));
-//        Company company = jobApplicationSystem.createCompany("Company");
-//        Branch branch = company.createBranch("Branch", "L4B4P8");
-//        Branch branch2 = company.createBranch("Branch2", "L4B4P8");
-//        Applicant applicant = jobApplicationSystem.getUserManager().createApplicant("username", "password", "Legal Name", "email@gmail.com", LocalDate.now(), "L4B4P8");
-//        HRCoordinator hrc = new HRCoordinator("HR", "password", "HRC", "email@gmail.com", branch, LocalDate.of(2019, 7, 19));
-//        HRCoordinator hrc2 = new HRCoordinator("HR2", "password", "HRC", "email@gmail.com", branch2, LocalDate.of(2019, 7, 19));
-//        BranchJobPosting jobPosting = hrc.addJobPosting("title", "field", "descriptionhujedkfnvsgrhjegskamkagjrwuiladkvmkajgirwouskvmzkjgiskdzvn,mkngs\niznvjgsirklzngjslitw4gsijlkznjsirtwtrsigjlzknvmJDEI0   IPUOwrahektdznmv\nlpox-98uy7gufhvnb tmwkeafoisCXU*yygchbvn    4mk2RWFsvzx\nwgudkngrhadkjn\nwhaegkjsc\ngwaeihfkncMZ<ghaecsknm,z\n",
-//                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(Arrays.asList("Tag1", "Tag2")), 1, LocalDate.of(2019, 7, 19), LocalDate.of(2019, 7, 19), LocalDate.of(2019, 7, 25));
-//        JobApplication jobApp = new JobApplication(applicant, jobPosting, LocalDate.of(2019, 7, 19));
-//        JobApplicationDocument doc = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        JobApplicationDocument doc2 = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        jobApp.addFiles(new ArrayList<>(Arrays.asList(doc, doc2)));
-//        Applicant applicant2 = jobApplicationSystem.getUserManager().createApplicant("username2", "password", "Legal Name2", "email@gmail.com", LocalDate.of(2019, 7, 19), "L4B4P8");
-//        BranchJobPosting jobPosting2 = hrc2.addJobPosting("title2", "field", "description",
-//                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(), 1, LocalDate.of(2019, 7, 20), LocalDate.of(2019, 7, 20), LocalDate.of(2019, 7, 26));
-//        JobApplication jobApp2 = new JobApplication(applicant2, jobPosting2, LocalDate.of(2019, 7, 19));
-//        reference.addJobApplication(jobApp);
-//        reference.addJobApplication(jobApp2);
-//        BranchJobPostingManager branchJobPostingManager = branch.getJobPostingManager();
-//        branchJobPostingManager.updateJobPostingsClosedForApplications(LocalDate.of(2019, 7, 21));
-//        branchJobPostingManager.updateJobPostingsClosedForReferences(LocalDate.of(2019, 7, 27));
-//        LogoutActionListener logoutActionListener = new LogoutActionListener(new Container(), new CardLayout(), jobApplicationSystem);
-//        frame.add(new HRMain(hrc, jobApplicationSystem, logoutActionListener));
-//        frame.setSize(854, 480);
-//        frame.setVisible(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    // Run this!
-    // This instantiates 2 applicants who apply to a single job posting
-    // Also instantiates an hr and interviewer in that company
-//        JobApplicationSystem jobApplicationSystem = new JobApplicationSystem();
-//        new DataLoaderAndStorer(jobApplicationSystem).loadAllData();
-//        jobApplicationSystem.setToday(LocalDate.of(2019, 7, 19));
-//        Company company = jobApplicationSystem.createCompany("Company");
-//        Branch branch = company.createBranch("Branch", "Toronto");
-//        Applicant applicant = jobApplicationSystem.getUserManager().createApplicant("username", "password", "Legal Name", "email@gmail.com", "L4B4P8", jobApplicationSystem);
-//        HRCoordinator hrc = new HRCoordinator("HR", "password", "HRC", "email@gmail.com", branch, LocalDate.of(2019, 7, 19));
-//        BranchJobPosting jobPosting = hrc.addJobPosting("title", "field", "descriptionhujedkfnvsgrhjegskamkagjrwuiladkvmkajgirwouskvmzkjgiskdzvn,mkngs\niznvjgsirklzngjslitw4gsijlkznjsirtwtrsigjlzknvmJDEI0   IPUOwrahektdznmv\nlpox-98uy7gufhvnb tmwkeafoisCXU*yygchbvn    4mk2RWFsvzx\nwgudkngrhadkjn\nwhaegkjsc\ngwaeihfkncMZ<ghaecsknm,z\n",
-//                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(), 1, LocalDate.of(2019, 7, 19), LocalDate.of(2019, 7, 19));
-//        JobApplication jobApp = new JobApplication(applicant, jobPosting, LocalDate.now());
-//        JobApplicationDocument doc = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        JobApplicationDocument doc2 = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        jobApp.addFiles(new ArrayList<>(Arrays.asList(doc, doc2)));
-//        Applicant applicant2 = jobApplicationSystem.getUserManager().createApplicant("username2", "password", "Legal Name2", "email@gmail.com", "L4B4P8", jobApplicationSystem);
-//        BranchJobPosting jobPosting2 = hrc.addJobPosting("title2", "field", "description",
-//                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(), 1, LocalDate.of(2019, 7, 20), LocalDate.of(2019, 7, 20));
-//        JobApplication jobApp2 = new JobApplication(applicant2, jobPosting2, LocalDate.of(2019, 7, 19));
-//        JobApplicationDocument doc3 = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
-//        jobApp2.addFiles(new ArrayList<>(Arrays.asList(doc3)));
-//        jobApplicationSystem.setToday(LocalDate.of(2019, 8, 25));
-//        new Interviewer("Interviewer", "password", "Bobby", "email", branch, "field", LocalDate.of(2019, 7, 10));
-//        jobApplicationSystem.updateAllJobPostings();
-//        LogoutActionListener logoutActionListener = new LogoutActionListener(new Container(), new CardLayout(), jobApplicationSystem);
-//        JFrame frame = new JFrame();
-//        frame.add(new HRMain(hrc, jobApplicationSystem, logoutActionListener));
-//        frame.setSize(854, 480);
-//        frame.setVisible(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    }
+    /*public static void main(String[] args) {
+        //Run this!
+        //This instantiates 2 applicants who apply to a single job posting
+        //Also instantiates an hr and interviewer in that company
+        JobApplicationSystem jobApplicationSystem = new JobApplicationSystem();
+        new DataLoaderAndStorer(jobApplicationSystem).loadAllData();
+        jobApplicationSystem.setToday(LocalDate.of(2019, 7, 19));
+        Company company = jobApplicationSystem.createCompany("Company");
+        Branch branch = company.createBranch("Branch", "Toronto");
+        Applicant applicant = jobApplicationSystem.getUserManager().createApplicant("username", "password", "Legal Name", "email@gmail.com", "L4B4P8", jobApplicationSystem);
+        HRCoordinator hrc = new HRCoordinator("HR", "password", "HRC", "email@gmail.com", branch, LocalDate.of(2019, 7, 19));
+        BranchJobPosting jobPosting = hrc.addJobPosting("title", "field", "descriptionhujedkfnvsgrhjegskamkagjrwuiladkvmkajgirwouskvmzkjgiskdzvn,mkngs\niznvjgsirklzngjslitw4gsijlkznjsirtwtrsigjlzknvmJDEI0   IPUOwrahektdznmv\nlpox-98uy7gufhvnb tmwkeafoisCXU*yygchbvn    4mk2RWFsvzx\nwgudkngrhadkjn\nwhaegkjsc\ngwaeihfkncMZ<ghaecsknm,z\n",
+                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(), 1, LocalDate.of(2019, 7, 19), LocalDate.of(2019, 7, 19));
+        JobApplication jobApp = new JobApplication(applicant, jobPosting, LocalDate.now());
+        JobApplicationDocument doc = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
+        JobApplicationDocument doc2 = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
+        jobApp.addFiles(new ArrayList<>(Arrays.asList(doc, doc2)));
+        Applicant applicant2 = jobApplicationSystem.getUserManager().createApplicant("username2", "password", "Legal Name2", "email@gmail.com", "L4B4P8", jobApplicationSystem);
+        BranchJobPosting jobPosting2 = hrc.addJobPosting("title2", "field", "description",
+                new ArrayList<>(Arrays.asList("CV", "Cover Letter", "Reference Letter")), new ArrayList<>(), 1, LocalDate.of(2019, 7, 20), LocalDate.of(2019, 7, 20));
+        JobApplication jobApp2 = new JobApplication(applicant2, jobPosting2, LocalDate.of(2019, 7, 19));
+        JobApplicationDocument doc3 = new JobApplicationDocument(new File("./sample.txt"), applicant.getUsername());
+        jobApp2.addFiles(new ArrayList<>(Arrays.asList(doc3)));
+        jobApplicationSystem.setToday(LocalDate.of(2019, 8, 25));
+        new Interviewer("Interviewer", "password", "Bobby", "email", branch, "field", LocalDate.of(2019, 7, 10));
+        jobApplicationSystem.updateAllJobPostings();
+        LogoutActionListener logoutActionListener = new LogoutActionListener(new Container(), new CardLayout(), jobApplicationSystem);
+        JFrame frame = new JFrame();
+        frame.add(new HRMain(hrc, jobApplicationSystem, logoutActionListener));
+        frame.setSize(854, 480);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }*/
 }
